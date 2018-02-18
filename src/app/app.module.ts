2577f2201aead227337078326bd9cee953ceb4bd
Björn Mosler <dev@bjoern.mosler.ch>
2018-02-18 14:53:42 +0100

    Add service worker.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
@@ -15,7 +17,8 @@ import { AgendaService } from './agenda.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [AgendaService],
   bootstrap: [AppComponent]
