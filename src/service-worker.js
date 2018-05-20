35d970dc884bd59138b2b3a1de77fc34398b30ba
Björn Mosler <dev@bjoern.mosler.ch>
2018-05-20 15:56:19 +0200

    Clean up service-worker.

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,24 +4,21 @@ let CACHE_NAME = 'wasgeit-cache-v1';
 let urlsToCache = [
     '/rest/agenda',
     '/rest/news',
-    '/script/main.js'
 ];
 
-self.addEventListener('install', function(event) {
-    // Perform install steps
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
+            .then(function (cache) {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-            .then(function(response) {
+            .then(function (response) {
                 // Cache hit - return response
                 if (response) {
                     return response;
@@ -31,12 +28,11 @@ self.addEventListener('fetch', function(event) {
                 // can only be consumed once. Since we are consuming this
                 // once by cache and once by the browser for fetch, we need
                 // to clone the response.
-                var fetchRequest = event.request.clone();
+                let fetchRequest = event.request.clone();
 
                 return fetch(fetchRequest).then(
-                    function(response) {
-                        // Check if we received a valid response
-                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                    function (response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
 
@@ -44,10 +40,10 @@ self.addEventListener('fetch', function(event) {
                         // and because we want the browser to consume the response
                         // as well as the cache consuming the response, we need
                         // to clone it so we have two streams.
-                        var responseToCache = response.clone();
+                        let responseToCache = response.clone();
 
                         caches.open(CACHE_NAME)
-                            .then(function(cache) {
+                            .then(function (cache) {
                                 cache.put(event.request, responseToCache);
                             });
 
