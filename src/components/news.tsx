2f69dbf340f23ad6c914863bd33dde1a542d558d
Björn Mosler <dev@bjoern.mosler.ch>
2018-05-20 11:27:29 +0200

    Use headings like 'gestern gefunden' etc instead of dates in news section.

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import {Event} from "../model/event"
-import {FormattedDate} from "./formatted-date"
 import {News} from "../model/news"
 import {Observable, Subscription} from "rxjs"
 import {ajax} from "rxjs/ajax"
@@ -32,12 +31,30 @@ const EventComponent = ({event}: { event: Event }) => {
 const DayComponent = ({day, events}: { day: string, events: Event[] }) => {
     return (
         <li>
-            <h3><FormattedDate isoDateString={day}/></h3>
+            <h3>{ buildTitle(day) }</h3>
             <ul>
                 {events.map((event: Event) => <EventComponent key={event.url} event={event}/>)}
             </ul>
         </li>
     )
+}
+
+
+function buildTitle(isoDate: string): string {
+    const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
+    let date = new Date(isoDate);
+    let today = new Date();
+    let diffInMs = today.getTime() - date.getTime();
+    let diffInDays = Math.floor(diffInMs / MS_IN_A_DAY);
+
+    switch (diffInDays) {
+        case 0:
+            return 'Heute gefunden';
+        case 1:
+            return 'Gestern gefunden';
+        default:
+            return `Vor ${diffInDays} Tagen gefunden`;
+    }
 }
 
 interface NewsListState {
