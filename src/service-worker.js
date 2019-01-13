6b2f8d24ff52abc39ea0cbab3cf459f5c97fb642
Björn Mosler <dev@bjoern.mosler.ch>
2019-01-13 19:25:50 +0100

    Update service worker to also fetch new data.
    Old version only returned cached values, duh.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-// Courtesy of https://developers.google.com/web/fundamentals/primers/service-workers/#you_need_https
+// Courtesy of https://serviceworke.rs/strategy-cache-and-update_demo.html
 
 let CACHE_NAME = 'wasgeit-cache-v1';
 let urlsToCache = [
@@ -6,50 +6,49 @@ let urlsToCache = [
     '/rest/news',
 ];
 
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
+
+    // Ask the service worker to keep installing until the returning promise
+    // resolves.
+    evt.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-
-                // IMPORTANT: Clone the request. A request is a stream and
-                // can only be consumed once. Since we are consuming this
-                // once by cache and once by the browser for fetch, we need
-                // to clone the response.
-                let fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function (response) {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
-    );
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
+    // You can use `respondWith()` to answer immediately, without waiting for the
+    // network response to reach the service worker...
+    evt.respondWith(fromCache(evt.request));
+    // ...and `waitUntil()` to prevent the worker from being killed until the
+    // cache is updated.
+    evt.waitUntil(update(evt.request));
 });
+
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.addAll(urlsToCache);
+    });
+}
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+}
